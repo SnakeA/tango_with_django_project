a3be503ede60b077ac0eb25f8c3aa1e39b5302dd
Angelos Constantinides <angelconstantinides@hotmail.com>
2014-11-21 02:59:09 +0000

    Chapter 5 Admin

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here!.
+class Category (models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    #method is therefore used to provide a unicode representation of a model instance
+    def __unicode__(self):
+        return self.name
+
+
+class Page(models.Model):
+    category=models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.title
+
+# Whenever you add to existing database models, you will have to delete the database
+# file and then re-sync the database by running python manage.py syncb again.
