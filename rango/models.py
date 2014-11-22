49d872b91ce576232fa64494a86b17718c761d85
Angelos Constantinides <angelconstantinides@hotmail.com>
2014-11-22 16:26:58 +0000

    Chapter 6 Finished!

--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Category (models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     #method is therefore used to provide a unicode representation of a model instance
     def __unicode__(self):
